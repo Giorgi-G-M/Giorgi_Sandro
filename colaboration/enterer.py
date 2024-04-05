415515b9ccf078ac7ca820193f4de9f1311bb638
SANDR111K <sanchesusinio@gmail.com>
2024-04-06 00:12:35 +0400

    enterer

--- colaboration/enterer.py
+++ colaboration/enterer.py
@@ -2,15 +2,15 @@ from calculator import main_calculator, numbers
 from to_do import main_to_do
 from money import main_currency
 from weather import weather_main
+from register import *
 
 def main():
+      # main_register()
       printer()
       choice = enter_program()
       orderer(choice)
 
-def register_logIn():
-    ...
-  
+
 def printer():
       print("Hello, this is a program where you can choose a task you need help with:")
       print(f"1. TO DO" +"\n"
@@ -19,7 +19,7 @@ def printer():
             f"4. Weather forecast" +"\n")
 
 def enter_program():
-      choice = input("Now choose from 1 to 4 which one you want to use: ")
+      choice = input("Now choose from 1 to 4 which one you want to use. or just click 'ENTER' adn program will end: ")
       return choice
 
 def orderer(choice):
@@ -28,17 +28,39 @@ def orderer(choice):
                   if choice == "1":
                         print("You are in 'TO DO' application")
                         main_to_do()
+                        printer()
+                        choice = enter_program()
+                        orderer(choice)
+
                   elif choice == "2":
                         print("You are in 'Calculator' application")
                         main_calculator(numbers())
+                        printer()
+                        choice = enter_program()
+                        orderer(choice)
+
                   elif choice == "3":
                         print("You are in currency 'Currency exchange' application")
                         main_currency()
+                        printer()
+                        choice = enter_program()
+                        orderer(choice)
+
                   elif choice == "4":
                         print("You are in 'Weather forecast' application")
                         weather_main()
+                        printer()
+                        choice = enter_program()
+                        orderer(choice)
+
+                  elif not choice:
+                        print("End proggram.")
+
                   else:
                         print("Something went wrong in your input.")
 
 if __name__ == "__main__":
       main()
+
+def squaree(x):
+      squared_x = x * x
